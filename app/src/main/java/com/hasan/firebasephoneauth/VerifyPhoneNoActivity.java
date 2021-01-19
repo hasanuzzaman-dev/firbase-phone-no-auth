575fa3f6f90000a80359d6c11bb3df83c7fa6ca3
Hasan <hasanuzzaman.dev@gmail.com>
2021-01-19 17:30:49 +0600

    phone auth working only testing number

--- app/src/main/java/com/hasan/firebasephoneauth/VerifyPhoneNoActivity.java
+++ app/src/main/java/com/hasan/firebasephoneauth/VerifyPhoneNoActivity.java
@@ -1,34 +1,177 @@
 package com.hasan.firebasephoneauth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 public class VerifyPhoneNoActivity extends AppCompatActivity {
+
+    private static final String TAG = "VerifyPhoneNoActivity";
 
     private Button verifyBtn;
     private EditText otpET;
     private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    private String verificationCodeBySystem;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_phone_no);
 
+        mAuth = FirebaseAuth.getInstance();
+
         verifyBtn = findViewById(R.id.verifyBtn);
         otpET = findViewById(R.id.otpET);
         progressBar = findViewById(R.id.progressBarID);
 
-        String phoneNo = getIntent().getStringExtra("phoneNon");
+        String phoneNo = getIntent().getStringExtra("phoneNo");
         sendVerificationCodeToUser(phoneNo);
+
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d(TAG, "onClick: btn");
+                String code = otpET.getText().toString();
+
+                if (code.isEmpty() || code.length() <6){
+                    otpET.setError("Wrong OTP");
+                    otpET.requestFocus();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                verifyCode(code);
+            }
+        });
+
+
 
 
     }
 
     private void sendVerificationCodeToUser(String phone) {
+        Log.d(TAG, "phone: "+phone);
+
+       // phone = "+8801713288234";
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phone)       // Phone number to verify
+                        .setTimeout(20L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+    }
+
+    private final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+            Log.d(TAG, "onVerificationCompleted: called");
+
+            String code = phoneAuthCredential.getSmsCode();
+            if (code != null) {
+                progressBar.setVisibility(View.VISIBLE);
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+            Log.d(TAG, "onVerificationFailed", e);
+            progressBar.setVisibility(View.GONE);
+
+            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+                // ...
+                Log.d(TAG, "onVerificationFailed: Invalid request");
+            } else if (e instanceof FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+                // ...
+                Log.d(TAG, "onVerificationFailed: The SMS quota for the project has been exceeded");
+            }
+
+            // Show a message and update the UI
+            // ...
+
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            verificationCodeBySystem = s;
+
+        }
+    };
+
+
+
+    private void verifyCode(String code) {
+
+        Log.d(TAG, "verifyCode: started");
+
+        PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(verificationCodeBySystem, code);
+        signInWithPhoneAuthCredential(phoneAuthCredential);
+
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        Log.d(TAG, "signInWithPhoneAuthCredential: started");
+
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success");
+
+                    FirebaseUser user = task.getResult().getUser();
+
+                    Intent intent = new Intent(VerifyPhoneNoActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    // ...
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.d(TAG, "signInWithCredential:failure", task.getException());
+                    if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                        Log.d(TAG, "onComplete: The verification code entered was invalid");
+                    }
+                }
+            }
+        });
 
     }
 }
